4ac0e5aa40dc7ee7ece3c671a24ef96dbdccfbbd
Brett Merrill <bmerrill@e1z2r5p16.42.us.org>
2017-04-08 23:48:35 -0700

    lowercase

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/03 15:03:46 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/04/08 23:47:06 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/04/08 23:48:21 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -180,6 +180,7 @@ char *print_p(va_list va, t_optional *options)
     arg = va_arg(va, void*);
     ret = ft_itoa_base(arg, 16);
     ret = ft_strjoin("0x", ret);
+    ret = ft_tolower_str(ret);
     return (ret);
 }
 
