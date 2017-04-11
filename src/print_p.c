d9b08222513d7b3d78b975509e13072e6f076973
Brett Merrill <bmerrill@e1z3r6p15.42.us.org>
2017-04-11 13:44:31 -0700

    no more p

--- src/print_p.c
+++ src/print_p.c
@@ -1,28 +1,28 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   print_p.c                                          :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/09 14:10:29 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/04/11 13:35:04 by bmerrill         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/* /\* ************************************************************************** *\/ */
+/* /\*                                                                            *\/ */
+/* /\*                                                        :::      ::::::::   *\/ */
+/* /\*   print_p.c                                          :+:      :+:    :+:   *\/ */
+/* /\*                                                    +:+ +:+         +:+     *\/ */
+/* /\*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        *\/ */
+/* /\*                                                +#+#+#+#+#+   +#+           *\/ */
+/* /\*   Created: 2017/04/09 14:10:29 by bmerrill          #+#    #+#             *\/ */
+/*   Updated: 2017/04/11 13:44:15 by bmerrill         ###   ########.fr       */
+/* /\*                                                                            *\/ */
+/* /\* ************************************************************************** *\/ */
 
-#include "ft_printf.h"
+/* #include "ft_printf.h" */
 
-char	*print_p(va_list va, t_optional *options)
-{
-	uintmax_t			arg;
-	char				*ret;
-	t_optional			*opt;
+/* char	*print_p(va_list va, t_optional *options) */
+/* { */
+/* 	uintmax_t			arg; */
+/* 	char				*ret; */
+/* 	t_optional			*opt; */
 
-	opt = options;
-	opt += 1;
-	arg = (uintmax_t)va_arg(va, void*);
-	ret = ft_itoa_base(arg, 16);
-	ret = ft_strjoin("0x", ret);
-	ret = ft_tolower_str(ret);
-	return (ret);
-}
+/* 	opt = options; */
+/* 	opt += 1; */
+/* 	arg = (uintmax_t)va_arg(va, void*); */
+/* 	ret = ft_itoa_base(arg, 16); */
+/* 	ret = ft_strjoin("0x", ret); */
+/* 	ret = ft_tolower_str(ret); */
+/* 	return (ret); */
+/* } */
