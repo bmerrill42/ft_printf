d1e0802e84f2c2fbc9d1edad4431feaa669844a5
Brett Merrill <bmerrill@e1z3r6p15.42.us.org>
2017-04-11 14:05:11 -0700

    free p

--- src/print_p.c
+++ src/print_p.c
@@ -6,7 +6,7 @@
 /*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:10:29 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/04/11 13:45:18 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/04/11 14:04:20 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 char	*print_p(va_list va, t_optional *options)
 {
-	/* uintmax_t			arg; */
-	/* char				*ret; */
-	/* t_optional			*opt; */
+	uintmax_t			arg;
+	char				*ret;
+	t_optional			*opt;
+	char 				*tmp;
 
-	/* opt = options; */
-	/* opt += 1; */
-	/* arg = (uintmax_t)va_arg(va, void*); */
-	/* ret = ft_itoa_base(arg, 16); */
-	/* ret = ft_strjoin("0x", ret); */
-	/* ret = ft_tolower_str(ret); */
-	return (NULL);
+	opt = options;
+	opt += 1;
+	arg = (uintmax_t)va_arg(va, void*);
+	ret = ft_itoa_base(arg, 16);
+	tmp = ret;
+	ret = ft_strjoin("0x", ret);
+	ft_strdel(tmp);
+	ret = ft_tolower_str(ret);
+	return (ret);
 }
