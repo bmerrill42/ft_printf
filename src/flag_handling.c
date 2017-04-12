317761982979fd5f39a8d8f17a7f91d34c95b368
Brett Merrill <bmerrill@e1z3r6p15.42.us.org>
2017-04-12 13:06:05 -0700

    no more frees

--- src/flag_handling.c
+++ src/flag_handling.c
@@ -6,7 +6,7 @@
 /*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:21:12 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/04/12 12:47:57 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/04/12 13:05:53 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ char	*padding(int size, char padding_char, int arg_length)
 
 char	*do_width(long long arg, char *ret, t_optional *options)
 {
-	char *tmp;
-
 	if (options->flags & WIDTH_FLAG && options->width > ft_strlen(ret))
 	{
 		if (options->flags & ZERO_FLAG && !(options->flags & MINUS_FLAG) && \
@@ -37,34 +35,22 @@ char	*do_width(long long arg, char *ret, t_optional *options)
 			if ((arg < 0 || options->flags & PLUS_FLAG) && \
 				ft_strlen(ret) < options->width)
 				ret[0] = '0';
-			tmp = ret;
 			ret = ft_strjoin(padding(options->width, '0', ft_strlen(ret)), ret);
-			free(tmp);
 			if (arg < 0)
 				ret[0] = '-';
 			if (options->flags & PLUS_FLAG && arg > 0)
 				ret[0] = '+';
 		}
 		if (options->flags & MINUS_FLAG)
-		{
-			tmp = ret;
 			ret = ft_strjoin(ret, padding(options->width, ' ', ft_strlen(ret)));
-			free(tmp);
-		}
 		else
-		{
-			tmp = ret;
 			ret = ft_strjoin(padding(options->width, ' ', ft_strlen(ret)), ret);
-			free(tmp);
-		}
 	}
 	return (ret);
 }
 
 char	*apply_flags(t_optional *options, char *ret, long long arg)
 {
-	char *tmp;
-
 	if (options->flags & SPACE_FLAG && (ret[0] != '-') && \
 		!(options->flags & WIDTH_FLAG))
 		ret = ft_strjoin(" ", ret);
@@ -74,9 +60,7 @@ char	*apply_flags(t_optional *options, char *ret, long long arg)
 			ret = "";
 		if (arg < 0)
 			ret[0] = '0';
-		tmp = ret;
 		ret = ft_strjoin(padding(options->precision, '0', ft_strlen(ret)), ret);
-		free(tmp);
 		if (arg < 0)
 			ret = ft_strjoin("-", ret);
 	}
@@ -85,11 +69,7 @@ char	*apply_flags(t_optional *options, char *ret, long long arg)
 		if (options->flags & SPACE_FLAG)
 			ret[0] = '+';
 		else
-		{
-			tmp = ret;
 			ret = ft_strjoin("+", ret);
-			free(tmp);
-		}
 	}
 	ret = do_width(arg, ret, options);
 	return (ret);
