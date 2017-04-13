77ed0d99e1699fd11a2e0705978417d306d0b616
Brett Merrill <bmerrill@e1z3r6p15.42.us.org>
2017-04-12 19:23:06 -0700

    protect

--- src/print_s.c
+++ src/print_s.c
@@ -6,7 +6,7 @@
 /*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 14:12:13 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/04/12 18:31:49 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/04/12 19:20:55 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 char	*print_s(va_list va, t_optional *options)
 {
 	char	*ret;
+	char 	*tmp;
 
-	ret = ft_strdup(va_arg(va, char*));
+	tmp = va_arg(va, char*);
+	if (tmp)
+		ret = ft_strdup(tmp);
 	if (options->flags & PRECISION_FLAG)
 		ret[options->precision] = '\0';
 	ret = do_width_u(1, ret, options);
