02c237975a16e899ddefcf58e38cdaf98f3a0464
Brett Merrill <bmerrill@e1z2r5p26.42.us.org>
2017-05-01 16:38:57 -0700

    proper hash on print_o

--- src/print_o.c
+++ src/print_o.c
@@ -6,7 +6,7 @@
 /*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 13:54:36 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/04/16 11:31:04 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/05/01 16:38:43 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ char	*print_o(va_list va, t_optional *options)
 	if (options->flags & HASH_FLAG)
 		ret = ft_strjoin_fr("0", ret);
 	ret = apply_flags_u(options, ret, arg);
-	if (options->flags & HASH_FLAG && options->width == 0)
-		ret = ft_strjoin_fr("0", ret);
 	return (ret);
 }
 
