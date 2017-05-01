77f10429f1ec32046832bd67b8abe4b999d4af6c
Brett Merrill <bmerrill@e1z2r5p26.42.us.org>
2017-05-01 16:40:30 -0700

    yes

--- src/print_o.c
+++ src/print_o.c
@@ -6,7 +6,7 @@
 /*   By: bmerrill <bmerrill@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/09 13:54:36 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/05/01 16:38:43 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/05/01 16:40:21 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*print_o(va_list va, t_optional *options)
 	arg = va_arg(va, uintmax_t);
 	arg = cast_length_u(arg, options);
 	ret = ft_itoa_base(arg, 8);
-	if (options->flags & HASH_FLAG)
+	if (options->flags & HASH_FLAG && arg > 0)
 		ret = ft_strjoin_fr("0", ret);
 	ret = apply_flags_u(options, ret, arg);
 	return (ret);
