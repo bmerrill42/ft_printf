ac424d9edbd582906f47d121fcd23dd9d0fc6a6f
Brett Merrill <bmerrill@e1z2r5p24.42.us.org>
2017-05-03 19:02:24 -0700

    even better space

--- src/apply_space.c
+++ src/apply_space.c
@@ -6,13 +6,13 @@
 /*   By: bmerrill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 18:55:47 by bmerrill          #+#    #+#             */
-/*   Updated: 2017/05/03 18:55:50 by bmerrill         ###   ########.fr       */
+/*   Updated: 2017/05/03 19:01:44 by bmerrill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void		apply_space(char **draft)
+void		apply_space(char **draft, intmax_t ref)
 {
 	int		i;
 	char	prefix_char;
@@ -22,7 +22,7 @@ void		apply_space(char **draft)
 	i = 0;
 	while (!ft_isdigit((*draft)[i]) && (*draft)[i] != '\0')
 		i++;
-	if (i > 0 && (*draft)[i - 1] == ' ')
+	if (i > 0 && (*draft)[i - 1] == ' ' || ref < 0)
 		return ;
 	else
 	{
